c905bda3b378a51961d2fd0cae57658e0d2ef0fb
Guilherme Donato <git@cadastro.dona.to>
2013-08-23 02:52:16 -0300

    That's pretty much it.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+$( document ).ready(function() {
+	$("button.forgetIt").click(function() {
+		$("textarea#secret").attr("placeholder", "Forget what? I've never seen you. ;)");
+	});
+	$("button.submit").click(function() {
+		$("form").slideUp();
+		$("div.done").fadeIn();
+	});
+	$("button.another").click(function() {
+		$("form").slideDown();
+		$("div.done").fadeOut();
+	});
+});
